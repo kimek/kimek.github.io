721fadff15f85ef0afb2601d18379c34e9267fe6
Matt Kimont <mateusz.kimont@gmail.com>
2024-09-09 22:31:10 +0100

    Add script to run local development environment and open page in chrome  - as I am lazy

---
+++ run-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Notes: https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
+google-chrome http://127.0.0.1:4000
+bundle exec jekyll serve
